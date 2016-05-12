de9fa115585ba8fffcd3b964466aa9d7fad77cb7
wubaofeng111 <18811429621@163.com>
2016-05-12 09:48:02 +0800

    添加 OC 字符串加法函数

--- SumOfLargeNum/main.m
+++ SumOfLargeNum/main.m
@@ -8,6 +8,45 @@
 
 #import <Foundation/Foundation.h>
 
+// OC 版大数据相加算法。
+
+NSString *sumWithStringAAndStringB(NSString* numA,NSString *numB)
+{
+    NSMutableString *resString = [NSMutableString string];
+    
+    NSInteger minLength = numA.length > numB.length ? numB.length :numA.length;
+    NSInteger maxLength = numA.length > numB.length ? numA.length : numB.length;
+    NSString *longString = numA.length > numB.length ? numA : numB;
+    NSString *shortString = numA.length > numB.length ? numB : numA;
+    NSInteger jinw = 0;
+
+    
+    NSInteger space = maxLength - minLength;
+    
+    for (NSInteger i = maxLength -1; i >= 0 ; i-- ) {
+        NSInteger shotIndex = i-space;
+        NSString *tempL = [longString substringWithRange:NSMakeRange(i, 1)];
+        NSString *tempS = nil;
+        
+        if (shotIndex >= 0) {
+            tempS =  [shortString substringWithRange:NSMakeRange(shotIndex, 1)];
+        }
+        NSInteger res = tempL.integerValue + tempS.integerValue + jinw;
+        jinw = 0;
+        if (res > 10) {
+            jinw = 1;
+        }
+        [resString insertString:[NSString stringWithFormat:@"%d",res] atIndex:0];
+        
+    }
+    return [resString copy];
+    
+    
+    
+    
+}
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
