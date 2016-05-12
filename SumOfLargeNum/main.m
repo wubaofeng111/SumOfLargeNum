72987407d6d998bf5b648386b0c60a8d68ef0e0b
friday <friday@fridaydeMacBook-Pro.local>
2016-05-12 09:24:03 +0800

    Initial Commit

---
+++ SumOfLargeNum/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SumOfLargeNum
+//
+//  Created by friday on 16/5/12.
+//  Copyright (c) 2016年 friday. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
